058a326c3927edc7974c481b4781c6913147c43e
rose-ele369 <rosa-234-1@hotmail.com>
2022-07-01 17:55:51 +0200

    Se añaden los últimos cambios de la calculadora

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import './App.css';
 import logoC from './imagenes/logo_conejo2.png'
 import Boton from './componentes/Boton';
+import Pantalla from './componentes/Pantalla';
+import BotonClear from './componentes/BotonClear';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 function App() {
+
+  const [input, setInput] = useState('');
+
+  const agregarInput = val => {
+    setInput( input + val );
+  };
+
+  const calcularResultado= () => {
+    if (input){
+      setInput(evaluate(input));
+    } else {
+      alert("Ingrese valores para realizar el cálculo");
+    }
+  }; 
+
   return (
     <div className="App">
       <div className='logo-contenedor'>
@@ -13,33 +32,36 @@ function App() {
       </div>
 
       <div className='contenedor-calculadora'>
+        <Pantalla input={input}/>
         <div className='fila'>
-          <Boton>1</Boton>
-          <Boton>2</Boton>
-          <Boton>3</Boton>
-          <Boton>+</Boton>
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
         </div>
         <div className='fila'>
-          <Boton>4</Boton>
-          <Boton>5</Boton>
-          <Boton>6</Boton>
-          <Boton>-</Boton>
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton manejarClic={agregarInput}>-</Boton>
         </div>
           
         <div className='fila'>
-          <Boton>7</Boton>
-          <Boton>8</Boton>
-          <Boton>9</Boton>
-          <Boton>*</Boton>
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>*</Boton>
         </div>
         <div className='fila'>
-          <Boton>=</Boton>
-          <Boton>0</Boton>
-          <Boton>.</Boton>
-          <Boton>/</Boton>
+          <Boton manejarClic={calcularResultado}>=</Boton>
+          <Boton manejarClic={agregarInput}>0</Boton>
+          <Boton manejarClic={agregarInput}>.</Boton>
+          <Boton manejarClic={agregarInput}>/</Boton>
         </div>
         <div className='fila'>
-          <Boton>Clear</Boton>
+          <BotonClear manejarClear={() => setInput('')}>
+            Clear
+          </BotonClear>
         </div>
       </div>
     </div>
--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -10,7 +10,8 @@ function Boton(props){
     return(
       /*Esta clase contiene javascript*/
       /*Las comillas invertidas sirven para crear Plantillas*/
-        <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}>
+        <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}
+        onClick={() => props.manejarClic(props.children)}>
           {props.children}
         </div>
     );
